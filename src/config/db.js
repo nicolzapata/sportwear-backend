a989bf5c054da190b5979526a61a1a39faa32cc7
nicolzapata <zapatanicol094@gmail.com>
2026-03-19 15:03:35 -0500

    cambios para visualizar bien en render

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,22 @@
+// src/config/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // 👈 CLAVE en Render
-
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false
-});
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        user:     process.env.DB_USER,
+        host:     process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port:     parseInt(process.env.DB_PORT) || 5432,
+      }
+);
 
 module.exports = pool;
