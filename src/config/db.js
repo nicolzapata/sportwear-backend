b44253c61eef57f363229c412ea76865f3b1797b
nicolzapata <zapatanicol094@gmail.com>
2026-03-18 19:50:42 -0500

    ajustar db para SSL

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
-  user:     process.env.DB_USER     || 'postgres',
-  host:     process.env.DB_HOST     || 'localhost',
-  database: process.env.DB_NAME     || 'SportWear',
-  password: process.env.DB_PASSWORD || 'zapata230707',
-  port:     parseInt(process.env.DB_PORT) || 5432,
+  connectionString: process.env.DATABASE_URL, // 👈 CLAVE en Render
+
+  ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : false
 });
 
 module.exports = pool;
- 
